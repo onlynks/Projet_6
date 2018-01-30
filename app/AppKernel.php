2d83ee657f7791370ebbc098badd0294a00bceb7
onlynks <nicogar12@gmail.com>
2018-01-31 00:02:27 +0100

    installation and bundle creation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new P6\PlatformBundle\P6PlatformBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
